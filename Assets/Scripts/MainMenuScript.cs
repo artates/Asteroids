55137ffe220a910f1b72aa33e894ae159449f7af
Nicolas  Artates <nartates9502@sdsu.edu>
2022-09-11 13:17:39 -0700

    added main menu controller object and script

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    //inits all buttons on the main menu, add more inits here if main menu grows
+    public Button playButton, settingsButton, quitButton;
+
+
+
+    //start, this is called before the first frame update
+    private void Start()
+    {
+        InitializeGameObjectsMenu();
+    }
+
+    /*
+     * initializes the game objects, called at the start
+     * uses gameobject.find to link the button in the scene to the button in the code
+     * adds listener to call LoadSceneByNumber
+    */
+    public void InitializeGameObjectsMenu()
+    {
+        //inits buttons
+        playButton = GameObject.Find("playButton").GetComponent<Button>();
+        settingsButton = GameObject.Find("settingsButton").GetComponent<Button>();
+        quitButton = GameObject.Find("quitButton").GetComponent<Button>();
+
+        //inits the listeners
+        playButton.onClick.AddListener(delegate { LoadSceneByNumber(0); });
+        settingsButton.onClick.AddListener(delegate { LoadSceneByNumber(0); });
+        quitButton.onClick.AddListener(delegate { QuitButtonCallback(); });
+    }
+
+
+    //Callback funtion for moving in between scenes
+    //0 = menu
+    public void LoadSceneByNumber(int SceneNumber)
+    {
+        Debug.Log("Scene Number index to be loaded is " + SceneNumber);
+        SceneManager.LoadScene(SceneNumber);
+    }
+
+    /*
+     * quit function, also exists within ChangeScene.cs(that one is more universal.
+     */
+    public void QuitButtonCallback()
+    {
+#if (UNITY_EDITOR)
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+    }
+}
