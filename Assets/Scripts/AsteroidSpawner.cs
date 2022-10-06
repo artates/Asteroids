d1b30b6e91c81771084afc0f67b77ed6dddf2f8a
Nicolas  Artates <nartates9502@sdsu.edu>
2022-10-06 16:59:57 -0700

    Changed init spawn time for ast to 3 sec. reduced bullet lifetime to 1.5 s increased asteroid size min 2.5 max 7. reduced speed to 20.0f.

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -7,7 +7,7 @@ public class AsteroidSpawner : MonoBehaviour
     //prefab for spawning
     public AsteroidScript asteroidPrefab;
 
-    private float initSpawn = 5.0f;
+    private float initSpawn = 3.0f;
     private float spawnRate = 20.0f;
     private int spawnAmount = 1;
     private float spawnDistance = 9.0f; //this is a hard code number, I want to change this to be relative to the screen bounds, maybe screen.width, screen.height
@@ -20,8 +20,8 @@ public class AsteroidSpawner : MonoBehaviour
 
     private void Spawn()
     {
-        //changes spawn amount to a random number between 1 and 3
-        this.spawnAmount = Random.Range(1, 4);
+        //changes spawn amount to a random number between 2 and 4
+        this.spawnAmount = Random.Range(2, 5);
 
         //logic for actually spawning them
         for ( int i = 0; i < this.spawnAmount; i++)
