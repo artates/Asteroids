cabef314496b0bb58687e129ad070b4f89d10ef1
artates <nicolasartates@gmail.com>
2024-05-20 19:50:12 -0700

    game over scene is now funtional.

--- Assets/Scripts/gameOverScript.cs
+++ Assets/Scripts/gameOverScript.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class gameOverScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //inits all buttons on the main menu, add more inits here if main menu grows
+    public Button playButtonGO, quitButtonGO;
+
+
+    //start, this is called before the first frame update
+    private void Start()
+    {
+        InitializeGameObjectsMenu();
+    }
+    /*
+         * initializes the game objects, called at the start
+         * uses gameobject.find to link the button in the scene to the button in the code
+         * adds listener to call LoadSceneByNumber
+        */
+    public void InitializeGameObjectsMenu()
     {
         
+        //inits buttons
+        playButtonGO = GameObject.Find("playButtonGO").GetComponent<Button>();
+        Debug.Log("init");
+        quitButtonGO = GameObject.Find("quitButtonGO").GetComponent<Button>();
+
+        //inits the listeners
+        playButtonGO.onClick.AddListener(delegate { LoadSceneByNumber(2); });
+        quitButtonGO.onClick.AddListener(delegate { QuitButtonCallback(); });
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //Callback funtion for moving in between scenes
+    //0 = menu
+    //2 = game
+    public void LoadSceneByNumber(int SceneNumber)
     {
-        
+        Debug.Log("Scene Number index to be loaded is " + SceneNumber);
+        SceneManager.LoadScene(SceneNumber);
+    }
+
+    /*
+     * quit function, also exists within ChangeScene.cs(that one is more universal.
+     */
+    public void QuitButtonCallback()
+    {
+#if (UNITY_EDITOR)
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
     }
 }
